73deba784830e39e981de359ec06186b874fa86e
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-02-28 07:51:11 -0300

    Migration e model User

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,43 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+class UserModel extends Model {
+  declare readonly id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  modelName: 'UserModel',
+  tableName: 'users',
+});
+
+export default UserModel;
